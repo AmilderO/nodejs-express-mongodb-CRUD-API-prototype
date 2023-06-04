b0fdcdecef5b7e7206fcf4c6c9aa6bbccc0482f3
AmilderO <aospinato@unal.edu.co>
2023-06-04 11:10:25 -0500

    Initial Commit

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var async = require('async');
-var Product = require('module/');
+var Product = require('../models/productModel');
 var router = express.Router();
 
 /* POST products listing. */
@@ -13,5 +13,56 @@ router.post('/', async(req, res) => {
         res.status(500).json({message: error.message});
     }
 });
+
+/* GET products listing. */
+router.get('/', async(req, res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error){
+        res.status(500).json({message: error.message})
+    }
+});
+
+/* GET product by ID. */
+router.get('/:id', async(req, res) =>{
+   try {
+       const {id} = req.params;
+       const product = await Product.findById(id);
+       res.status(200).json(product)
+   } catch (error) {
+       res.status(500).json({message: error.message})
+   }
+});
+
+/* UPDATE product by ID. */
+router.put('/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message: "No se puede encontrar un producto con ID = " + id})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+/* DELETE product by ID. */
+router.delete('/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: "No se puede encontrar un producto con ID = " + id})
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
 
 module.exports = router;
